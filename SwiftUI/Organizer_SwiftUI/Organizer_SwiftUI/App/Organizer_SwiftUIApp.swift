b38eec7b101bc263fce5509487d7eb6908ea29cc
Thibaut Richez <thibautrichez@hotmail.fr>
2023-10-11 18:01:10 +0200

    Add SwiftUI project

---
+++ SwiftUI/Organizer_SwiftUI/Organizer_SwiftUI/App/Organizer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Organizer_SwiftUIApp.swift
+//  Organizer_SwiftUI
+//
+//  Created by Thibaut Richez on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Organizer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
