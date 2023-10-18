1538cd9a9334026996d9eb6a9c803216770f7f6a
Thibaut Richez <thibautrichez@hotmail.fr>
2023-10-18 16:08:20 +0200

    Add macOS target

---
+++ SwiftUI/Organizer_SwiftUI/MacOrganizer/MacOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOrganizerApp.swift
+//  MacOrganizer
+//
+//  Created by Thibaut Richez on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MacOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
