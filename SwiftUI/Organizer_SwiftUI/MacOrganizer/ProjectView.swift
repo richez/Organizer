e515ba69bfe33fc674479c571c9d224f885cf619
Thibaut Richez <thibautrichez@hotmail.fr>
2023-10-18 16:29:14 +0200

    Add App file to mac target

---
+++ SwiftUI/Organizer_SwiftUI/MacOrganizer/ProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectView.swift
+//  MacOrganizer
+//
+//  Created by Thibaut Richez on 18/10/2023.
+//
+
+import SwiftUI
+
+struct ProjectView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ProjectView()
+}
