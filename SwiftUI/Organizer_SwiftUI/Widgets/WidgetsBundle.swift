d88ff5db63fc90f65114eabd4705e4ef732f1532
Thibaut Richez <thibautrichez@hotmail.fr>
2023-10-26 01:39:06 +0200

    Add Widget target for iOS and macOS

---
+++ SwiftUI/Organizer_SwiftUI/Widgets/WidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by Thibaut Richez on 26/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+    }
+}
